9abac2f2d943bade4e500e5b0c1f0e3948806f74
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-12-03 15:35:13 +0200

    Completed day 3 part 2

--- day-3/solution.py
+++ day-3/solution.py
@@ -1,2 +1,5 @@
 # Part 1
 print(sum([ord(item[0]) - ord('a') + 1 if item[0].islower() else ord(item[0]) - ord('A') + 27 for item in [list(set(rucksack[:len(rucksack)//2]).intersection(rucksack[len(rucksack)//2:])) for rucksack in open("input.txt").read().splitlines()]if len(item) > 0]))
+
+# Part 2
+with open("input.txt") as file: lines=file.read().splitlines(); print(sum([ord(item[0]) - ord('a') + 1 if item[0].islower() else ord(item[0]) - ord('A') + 27 for item in [list(set(lines[i]).intersection(lines[i+1]).intersection(lines[i+2])) for i in range(0, len(lines), 3)]]))
