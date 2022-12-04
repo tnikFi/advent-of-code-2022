e1c26552c5b6a3e5f8753ef608e11a8421d29fae
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-12-04 15:47:16 +0200

    Completed day 4 part 2

--- day-4/solution.py
+++ day-4/solution.py
@@ -1,2 +1,5 @@
 # Part 1
 print(len([x for x in [[sections[0].split("-"), sections[1].split("-")] for sections in [pair.split(",") for pair in open("input.txt").read().splitlines()]] if (int(x[0][0]) >= int(x[1][0]) and int(x[0][1]) <= int(x[1][1])) or ((int(x[0][0]) <= int(x[1][0]) and int(x[0][1]) >= int(x[1][1])))]))
+
+# Part 2
+print(len([x for x in [[sections[0].split("-"), sections[1].split("-")] for sections in [pair.split(",") for pair in open("input.txt").read().splitlines()]] if int(x[0][0]) <= int(x[1][1]) and int(x[0][1]) >= int(x[1][0])]))
